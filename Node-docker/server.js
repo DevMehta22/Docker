76eb30e671f1fe123fe865339af0eb5d2a13326f
DevMehta22 <mehtadev907@gmail.com>
2024-02-04 23:59:49 +0530

    node-docker 2.0

--- Node-docker/server.js
+++ Node-docker/server.js
@@ -3,7 +3,7 @@ const express  =require("express")
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("<h1>Hello baby User!!</h1>");
+    res.send("<h1>Hello User!!</h1>");
 })
 
 app.listen(process.env.PORT || 3000,(err)=>{
